c4719daa78cc9f56d2c2bbfd5635b04917965073
muslumdogan01 <muslumdogan01@gmail.com>
2022-02-09 20:39:14 +0300

    date edit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,11 +3,3 @@ export function withDomain(uri) {
 }
 
 
-// convert createdAt: "2022-01-17T06:36:03.773Z" to "17 January 2022"
-export function convertDate(date) {
-  const newDate = new Date(date);
-  const day = newDate.getDate();
-  const month = newDate.getMonth();
-  const year = newDate.getFullYear();
-  return `${day} ${month} ${year}`;
-}
